816cd369764fa4e59f5bdce1f7c7967d22613d2b
PaulDenis <d.paul@outlook.it>
2021-04-30 17:20:46 +0200

    Esercizio completo

--- js/script.js
+++ js/script.js
@@ -14,9 +14,23 @@ var cognomiMaiuscoli = [];
 for (var i=0; i < cognomi.length; i++) {
     cognomiMaiuscoli.push(cognomi[i].toUpperCase());
 }
-console.log(cognomi);
+// console.log(cognomi);
 console.log(cognomiMaiuscoli);
 
 cognomiMaiuscoli.sort();
 console.log(cognomiMaiuscoli);
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+var check = false;
+var y = 0;
+
+while ( y < cognomiMaiuscoli.length || check == false) {
+    if (tuoCognome.toUpperCase() == cognomiMaiuscoli[y]) {
+        y=y+1;
+        console.log("Il tuo cognome è in posizione " + y);
+        check = true;
+    } else {
+        // console.log("non è qui");
+        y++
+    }
+}
+// console.log(y-1);
