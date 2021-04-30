db3e2a2d6aee8f0d59a0f2e0027b41c47a7ba4bb
PaulDenis <d.paul@outlook.it>
2021-04-30 17:50:51 +0200

    Fixati dei piccoli buggerini

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
-var cognomi = ["Paul", "Podghirnii", "Gherardi", "Constantinescu", "Luppi", "Astoni"];
+var cognomi = [" Paul", " Podghirnii", " Gherardi", " Constantinescu", " Luppi", " Astoni"];
+
 
 // 1. chiedi all'utente il cognome
 
 var tuoCognome = prompt("Inserisci il tuo cognome:");
-
+tuoCognome = " " + tuoCognome
 // 2. inseriscilo in un array con altri cognomi: 'Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'
 
 cognomi.push(tuoCognome);
+document.getElementById("tuo_cognome").innerHTML = tuoCognome;
 
 // 3. stampa la lista ordinata alfabeticamente
 var cognomiMaiuscoli = [];
@@ -19,6 +21,12 @@ console.log(cognomiMaiuscoli);
 
 cognomiMaiuscoli.sort();
 console.log(cognomiMaiuscoli);
+
+for (i=0; i< cognomiMaiuscoli.length; i++) {
+document.getElementById("nuova_lista").innerHTML += (i+1) +  cognomiMaiuscoli[i] + "<br>";
+}
+
+
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
 var check = false;
 var y = 0;
@@ -27,10 +35,11 @@ while ( y < cognomiMaiuscoli.length || check == false) {
     if (tuoCognome.toUpperCase() == cognomiMaiuscoli[y]) {
         y=y+1;
         console.log("Il tuo cognome è in posizione " + y);
+        document.getElementById("posizione").innerHTML = "Il tuo cognome è in posizione: " + y;
+
         check = true;
     } else {
-        // console.log("non è qui");
+        console.log("non è qui");
         y++
     }
 }
-// console.log(y-1);
